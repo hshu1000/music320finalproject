7006f53b97db353efa4969e28371d56ba9e8dff3
luke qiao <LukeQiao2222@gmail.com>
2026-03-06 23:25:23 -0800

    added ref violin spectrogram

--- diagrams.py
+++ diagrams.py
@@ -4,17 +4,19 @@ import librosa
 import librosa.display
 
 # Config
-AUDIO_FILE   = "recordings/piano.wav" 
+AUDIO_FILE   = "recordings/reference_violin.m4a" 
 DPI          = 300
 N_FFT        = 2048
 HOP_LENGTH   = 256
 
+fs = 44.1e3
 y, sr = librosa.load(AUDIO_FILE, sr=None)
 
+y = y[int(3*fs):int(5*fs)]
 S = librosa.stft(y, n_fft=N_FFT, hop_length=HOP_LENGTH)
 S_db = librosa.amplitude_to_db(np.abs(S), ref=np.max)
 
-plt.figure(figsize=(16, 9))
+plt.figure(figsize=(10, 8))
 
 librosa.display.specshow(
     S_db,
@@ -26,6 +28,6 @@ librosa.display.specshow(
 )
 
 plt.colorbar(format="%+2.0f dB")
-plt.title("Piano Spectrogram")
+plt.title("Violin Spectrogram")
 plt.ylim(0,10000)
 plt.show()
