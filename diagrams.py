1df43a5d89cee21c8aec584d349e7bdb6b4a946d
Mona Lisa <hshu100@stanford.edu>
2026-03-15 17:41:14 -0700

    Recommit local changes without virtual environment

--- diagrams.py
+++ diagrams.py
@@ -4,7 +4,12 @@ import librosa
 import librosa.display
 
 # Config
+<<<<<<< HEAD
+AUDIO_FILE   = "cherie_violin.m4a"
+OUTPUT_FILE  = "cherie_violin_spectrogram.png"
+=======
 AUDIO_FILE   = "recordings/reference_violin.m4a" 
+>>>>>>> 3cd0966225cc424b73a76bc8da2a0d690f0e481e
 DPI          = 300
 N_FFT        = 2048
 HOP_LENGTH   = 256
@@ -16,7 +21,11 @@ y = y[int(3*fs):int(5*fs)]
 S = librosa.stft(y, n_fft=N_FFT, hop_length=HOP_LENGTH)
 S_db = librosa.amplitude_to_db(np.abs(S), ref=np.max)
 
+<<<<<<< HEAD
+plt.figure(figsize=(10, 9), dpi=DPI)
+=======
 plt.figure(figsize=(10, 8))
+>>>>>>> 3cd0966225cc424b73a76bc8da2a0d690f0e481e
 
 librosa.display.specshow(
     S_db,
@@ -29,5 +38,14 @@ librosa.display.specshow(
 
 plt.colorbar(format="%+2.0f dB")
 plt.title("Violin Spectrogram")
+<<<<<<< HEAD
+plt.ylim(0, 10000)
+
+# Save figure
+plt.savefig(OUTPUT_FILE, dpi=DPI, bbox_inches="tight")
+
+plt.close()
+=======
 plt.ylim(0,10000)
 plt.show()
+>>>>>>> 3cd0966225cc424b73a76bc8da2a0d690f0e481e
